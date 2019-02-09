2b8cd6d418457ddef0e61b18af3191af9e6e83a2
Antonio R. Collins II <ramar.collins@gmail.com>
2019-02-09 15:26:08 -0500

    Kept adding to the streamer/parser tool.

--- p.c
+++ p.c
@@ -3,7 +3,7 @@ cwbuilder
 
 Builds the database for CheapWhips.net...
  * ---------------------------------------------- */
-
+#if 0 
 #include <stdio.h> 
 #include <unistd.h> 
 #include <errno.h> 
@@ -11,10 +11,14 @@ Builds the database for CheapWhips.net...
 #include <sys/types.h> 
 #include <fcntl.h> 
 #include <string.h> 
+#endif
+#include "vendor/single.h"
 #include <gumbo.h>
 #define PROG "p"
 
 
+static unsigned int ind=0;
+char *gumbo_type = NULL;
 const char *html_1 = "<h1>Hello, World!</h1>";
 const char html_file[] = "files/carri.html";
 const char yamama[] = ""
@@ -22,40 +26,16 @@ const char yamama[] = ""
 	"<body>"
 		"<h2>Snazzy</h2>"
 		"<p>The jakes is coming</p>"
+		"<div>"
+			"<p>The jakes are still chasing me</p>"
+			"<div>"
+				"<p>Son, why these dudes still chasing me?</p>"
+			"</div>"
+		"</div>"
 	"</body>"
 "</html>"
 ;
 
-/*
-1; json
-html {
-	head { ... }
-	body {
-		h2
-		p
-	}
-}
-
-*/
-
-/*
-//An example iterator
-void mit ( GumboNode *node ) {
-	GumboVector *vc = &node->v.element.children;
-
-	for ( unsigned int i=0; i < vc->length; i++ ) {
-		//Print the type of node and the content (if printable)	
-		fprintf( stderr, "%d -> %s %p\n", i, 
-			gumbo_normalized_tagname( (vc->data[ i ]).type ), 
-			vc->data[ i ] );
-	}
-}
-*/
-
-
-
-static unsigned int ind=0;
-char *gumbo_type = NULL;
 
 char *print_gumbo_type ( GumboNodeType t ) {
 	if ( t == GUMBO_NODE_DOCUMENT )
@@ -100,6 +80,52 @@ GumboNode* find_tag ( GumboNode *node, GumboTag t ) {
 }
 
 
+//Go through and run something on a node and ALL of its descendants
+int rr ( GumboNode *node ) {
+	//Loop through the body and create a "Table" 
+	GumboVector *bc = &node->v.element.children;
+
+	//???
+	for ( int i=0; i<bc->length; i++ ) {
+		//Get data "endpoints"
+		GumboNode *n = bc->data[ i ] ;
+		const char *tagname = gumbo_normalized_tagname( n->v.element.tag );
+		const char *type = (char *)print_gumbo_type( n->type );
+
+		//Dump everything
+		fprintf( stderr, "%02d, %-10s, %s\n", i, type, tagname );
+
+		//Check if there's children, if so, descend, if not, print what's there 
+		if ( n->type != GUMBO_NODE_ELEMENT ) {
+			//what do I do?
+		}
+		else {
+			//get a count of the children?	
+			GumboVector *gv = &n->v.element.children;
+			//GumboNode *nn = gv->data[ 0 ];
+			//fprintf( stderr, "%d\n", gv->length );
+			//regardless of tag type, all attributes need to be saved, not sure how
+			//to loop through this yet
+
+			//then, keep running and going deeper in the tree until it's all parsed
+			if ( !gv->length ) {
+			//fprintf( stderr, "%s\n", gumbo_normalized_tagname( nn->v.element.tag ) ); 
+			//fprintf( stderr, "%s\n", print_gumbo_type( nn->type ) );
+				//img, or some other unclosable tag
+			}
+			else if ( gv->length == 1 ) {
+
+			}
+			else {
+				//re-run for each node?
+				rr( n );
+			}	
+		}	
+	}
+
+	return 0;
+}
+
 
 //Much like moving through any other parser...
 int main() {
@@ -141,6 +167,10 @@ int main() {
 	GumboVector *children = &(output->root)->v.element.children;
 	GumboNode *body = find_tag( output->root, GUMBO_TAG_BODY );
 	//GumboNode *head = find_tag( output->root, GUMBO_TAG_HEAD );
+	if ( !body ) {
+		fprintf( stderr, PROG ": no <body> found!\n" );
+		return 1;
+	}
 
 	//Loop through the body and create a "Table" 
 	GumboVector *bc = &body->v.element.children;
@@ -149,8 +179,26 @@ int main() {
 		GumboNode *gn = bc->data[ i ] ;
 		const char *gtagname = gumbo_normalized_tagname( gn->v.element.tag );
 		const char *gtype = (char *)print_gumbo_type( gn->type );
+
 		//Dump everything
 		fprintf( stderr, "%02d, %-10s, %s\n", i, gtype, gtagname );
+
+		//Check if there's children, if so, descend, if not, print what's there 
+		if ( gn->type != GUMBO_NODE_ELEMENT ) {
+			//what do I do?
+		}
+		else {
+			//get a count of the children?	
+			GumboVector *gv = &gn->v.element.children;
+			fprintf( stderr, "%d\n", gv->length );
+			//if it's over 1, then we'll loop thru
+			//if it's 1, what type is the node? 
+			GumboNode *nn = gv->data[ 0 ];
+			fprintf( stderr, "%s\n", gumbo_normalized_tagname( nn->v.element.tag ) ); 
+			fprintf( stderr, "%s\n", print_gumbo_type( nn->type ) );
+			//fprintf( stderr, "%s\n", nn->v.text.text );
+		}	
+		const char *gtext = (char *)print_gumbo_type( gn->type );
 	}
 
 
